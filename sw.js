9d9314db94ae52c4da115e7bebc2362ffab2dfb9
FernandoDelgadoDiaz <fernandogastondelgado81@gmail.com>
2025-08-26 18:41:04 -0300

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,5 @@
-self.addEventListener("install", () => {
-  self.skipWaiting();
-});
-self.addEventListener("activate", () => {
-  console.log("Service Worker activo para EstimaPres");
-});
-
+// sw.js (service worker "neutral")
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => self.clients.claim());
+// No interceptamos nada: todo va directo a la red (sin caché)
+self.addEventListener('fetch', () => {});
